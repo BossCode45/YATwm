b1bc250b21e69cccf3c32058352237bd04a01319
BossCode45 <boss@tehbox.org>
2025-05-11 18:17:27 +1200

    version-bump: Bumping version to try fix nix

--- YATwm.nix
+++ YATwm.nix
@@ -11,11 +11,11 @@
 
 stdenv.mkDerivation {
     pname = "YATwm";
-    version = "0.0.1";
+    version = "0.0.2";
 
     src = fetchgit {
         url = "https://git.tehbox.org/cgit/boss/YATwm.git/";
-        rev = "v0.0.1";
+        rev = "v0.0.2";
         hash = "sha256-A4Yra/903rOeEbXfFia/A2HRPrFyE1b05mzHWlDImCY=";
     };
 
